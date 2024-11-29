a4c6770eb105643031c6b36f97f971984a1ba4c4
xnto8 <asanchezm9@correo.uss.cl>
2024-11-29 16:22:48 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -98,7 +98,7 @@ def graficos():
 
     if st.button("Generar Gráfico"):
         if selected_columns and chart_type != "Selecciona una opción":
-            fig, ax = st.pyplot(subplots())
+            fig, ax = st.subplots()
             if chart_type == "Línea":
                 for col in selected_columns:
                     ax.plot(data.index, data[col], label=col)
